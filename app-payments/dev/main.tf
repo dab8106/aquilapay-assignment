5a38b63d533b135339e7068ae5014304a87a4624
Dabeer Shaikh <dab8106@gmail.com>
2025-12-23 22:27:50 +0530

    fix

--- app-payments/dev/main.tf
+++ app-payments/dev/main.tf
@@ -1,7 +1,6 @@
 provider "google" {
   project     = var.project_id
   region      = var.region
-  credentials = base64decode(var.google_credentials)
 }
 
 module "network" {
--- app-payments/dev/variables.tf
+++ app-payments/dev/variables.tf
@@ -3,8 +3,3 @@ variable "region" {}
 variable "vpc_name" {}
 variable "subnet_name" {}
 variable "cidr" {}
-variable "google_credentials" {
-  description = "Base64 encoded GCP credentials"
-  type        = string
-  sensitive   = true
-}
