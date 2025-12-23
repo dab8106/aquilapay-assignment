e91f01a03b744ca968171db0d8ccda2af465178b
Dabeer Shaikh <dab8106@gmail.com>
2025-12-23 21:43:40 +0530

    credentials

--- app-payments/dev/terraform.tf
+++ app-payments/dev/terraform.tf
@@ -1,3 +1,9 @@
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  credentials = base64decode(var.google_credentials)
+}
+
 terraform {
   cloud {
     organization = "aquilapay-org"
--- app-payments/dev/variables.tf
+++ app-payments/dev/variables.tf
@@ -3,3 +3,8 @@ variable "region" {}
 variable "vpc_name" {}
 variable "subnet_name" {}
 variable "cidr" {}
+variable "google_credentials" {
+  description = "Base64 encoded GCP credentials"
+  type        = string
+  sensitive   = true
+}
